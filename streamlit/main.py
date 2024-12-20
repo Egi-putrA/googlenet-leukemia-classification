8cc2427deb35408a7d356ce55c4a8eec869eaf63
egiputra <egiputra450@gmail.com>
2024-12-20 17:42:44 +0700

    use clahe model adding clahe preprocessing

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,17 +1,23 @@
 import math
 
 import streamlit as st
+import numpy as np
 import tensorflow as tf
+import cv2
 
 class_names = ['Early', 'Benign', 'Pre', 'Pro']
 
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+
 @st.cache_resource
 def load_model():
-    return tf.saved_model.load('googlenet_model/original_data').signatures["serving_default"]
+    return tf.saved_model.load('googlenet_model/clahe_balanced_model').signatures['serving_default']
 
 model = load_model()
 
 images_uploaded = st.file_uploader("Upload file", type=['jpg', 'png'], accept_multiple_files=True)
+
+
 
 for i in range(math.ceil(len(images_uploaded) / 3)):
     cols = min(3, len(images_uploaded) - (i*3))
@@ -19,11 +25,26 @@ for i in range(math.ceil(len(images_uploaded) / 3)):
 
     for j in range(cols):
         row[j].image(images_uploaded[i*3 + j], width=224)
-        img = tf.io.decode_image(images_uploaded[i*3 + j].read(), dtype="uint8", channels=3)
-        img = tf.image.resize(img, [224, 224])
+
+        # read image
+        arr = np.asarray(bytearray(images_uploaded[i*3 + j].read()), dtype=np.uint8)
+        print(arr)
+        img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+        
+        # clahe preprocessing
+        lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+
+        l = clahe.apply(l)
+        lab = cv2.merge([l, a, b])
+
+        img = cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+        
+        # resize and normalization (from int to float 0-1)
+        img = cv2.resize(img, (224, 224))
         img = tf.convert_to_tensor([img / 255], dtype=tf.float32)
 
-
+        # predict
         pred = model(img)
         pred = tf.math.argmax(pred['result'], axis=1).numpy().tolist()
         row[j].write(class_names[pred[0]])
